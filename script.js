db389e256fa6ce33714ce5a605d1117afb1f3d02
PrakashUpadhyay-design <prakashu788@gmail.com>
2025-03-23 11:37:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let popup = document.getElementById('welcomePopup');
+//     popup.style.visibility = 'visible';
+//     popup.style.opacity = '1';
+// });
+// function closePopup() {
+//     let popup = document.getElementById('welcomePopup');
+//     popup.style.opacity = '0';
+//     setTimeout(() => {
+//         popup.style.visibility = 'hidden';
+//     }, 500);
+// }
